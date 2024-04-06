bc61958a379e8832081d65701db53f1f75211c44
Ana Rius <a.riusvi11@gmail.com>
2024-04-06 08:53:53 +0200

    fea: redux template installed

---
+++ app/store.js
@@ -0,0 +1,30 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import { encryptTransform } from 'redux-persist-transform-encrypt';
+import { thunk } from "redux-thunk";
+
+import storage from "redux-persist/lib/storage";
+
+import userSlice from "./slices/userSlice";
+
+
+const reducers = combineReducers({
+    user: userSlice,
+});
+
+const persistConfig = {
+    key: "root",
+    storage, 
+};
+
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export default configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }).concat(thunk),
+});
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//REDUX
+
+import { Provider } from "react-redux";
+import  store  from "../app/store.js";
+
+//REDUX PERSISTENCE
+
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+//PERSISTOR
+const persistor = persistStore(store);
+
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 )
