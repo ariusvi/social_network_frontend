bc0847cc67471ac53ee16dd510951107f1bb7a41
Ana Rius <a.riusvi11@gmail.com>
2024-04-10 14:34:49 +0200

    feat: delete updateProfile from profile.jsx to be fixed later

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -4,9 +4,8 @@ import { useNavigate } from 'react-router-dom';
 import { userData } from "../../app/slices/userSlice";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
-import { getMyPosts, updateProfile } from "../../services/apiCalls";
+import { getMyPosts} from "../../services/apiCalls";
 import selloPost from '../../img/sello_post.png'
-import {CButton} from '../../common/CButton/CButton'
 
 
 export const Profile = () => {
@@ -27,9 +26,6 @@ export const Profile = () => {
 
     const [myPosts, setMyPosts] = useState([]);
 
-    const [loadedData, setLoadedData] = useState(false);
-    const [write, setWrite] = useState("disabled");
-
     useEffect(() => {
         const getMyPostsData = async () => {
             try {
@@ -43,86 +39,23 @@ export const Profile = () => {
     }, [myPosts, token])
 
 
-    const [user, setUser] = useState({
-        nickname: "",
-        biography: "",
-        avatar: "",
-    })
-
-    const [userError, setUserError] = useState({
-        nicknameError: "",
-        biographyError: "",
-        avatarError: "",
-    });
-
-    const inputHandler = (e) => {
-        setUser(
-            (prevState) => ({
-                ...prevState,
-                [e.target.name]: e.target.value
-            })
-        )
-    };
-
-    useEffect(() => {
-        const getUserData = async () => {
-            try {
-                const fetched = await getUserData(token)
-                setLoadedData(true)
-
-                setUser({
-                    nickname: fetched.data.nickname,
-                    biography: fetched.data.biography,
-                    avatar: fetched.data.avatar,
-                })
-            }
-            catch (error) {
-                console.log(error)
-            }
-        }
-
-    }, [])
-
-    const updateUserData = async () => {
-        try {
-            const fetched = await updateProfile(token, user)
-            setUser({
-                nickname: fetched.data.nickname,
-                biography: fetched.data.biography,
-                avatar: fetched.data.avatar,
-            })
-            setWrite("disabled")
-        } catch (error) {
-            console.log(error)
-        }
-
-    }
-
-
     return (
         <>
             <div className='profileDesign'>
                 <div className="paperProfile">
                     <div className='profileData'>
-                        {!loadedData ? <div>Loading...</div> :
+                        <div>
+                            <div></div>
                             <div>
-                                <div></div>
-                                <div>
-                                    <img
-                                        className="avatarProfile"
-                                        src={reduxUser.credentials.user.avatar}
-                                        alt="user's avatar">
-                                    </img>
-                                    <div className="nickname">{reduxUser.credentials.user.nickname}</div>
-                                </div>
-                                <div>{reduxUser.credentials.user.biography}</div>
-                                <CButton 
-                                className={write === "" ? "CustomButtonDesign" : "CustomButtonDesign"}
-                                title={write === "" ? "Confirmar" : "Editar"}
-                                functionEmit={write === "" ? () => updateUserData() : () => setWrite("")}
-                                />
+                                <img
+                                    className="avatarProfile"
+                                    src={reduxUser.credentials.user.avatar}
+                                    alt="user's avatar">
+                                </img>
+                                <div className="nickname">{reduxUser.credentials.user.nickname}</div>
                             </div>
-                        }
+                            <div>{reduxUser.credentials.user.biography}</div>
+                        </div>
                     </div>
                 </div>
                 <div><img className='newPost' src={selloPost} alt="Sello Post" onClick={() => navigate('/newpost')} /></div>
@@ -142,7 +75,6 @@ export const Profile = () => {
                         )
                     })}
                 </div>
-
             </div>
         </>
     )
